94b9e17a05b1565703bdeeee992b9af53562fb01
Ross Fadely <rossfadely@gmail.com>
2015-11-07 18:57:09 +0000

    debugging conditionals

--- est_cost.py
+++ est_cost.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as pl
 
 from sklearn.mixture import GMM
-gtom sklearn.neighbors import KernelDensity
+from sklearn.neighbors import KernelDensity
 
 def fit_gmm(costs, k=3, min_samps=50):
     """
@@ -30,12 +30,22 @@ def select_conditionals(df, features):
     """
     Return the costs associated with the selected features.
     """
-    costs = np.zeros(df['costs'])
+    costs = np.array(df['Total Charges'])
     keys = features.keys()
-    ind = np.bool(df.shape[0])
-    for i in range(1, len(keys)):
+    ind = np.zeros(df.shape[0], np.bool)
+    print features
+    print ind.shape
+    print keys
+    #arr = np.array(df[keys[0]])
+    print arr.shape
+    for i in range(len(keys)):
         arr = np.array(df[keys[i]], type(features[keys[i]]))
         ind = ind & (arr == features[keys[i]])
+
+    print arr[ind].shape
+    print costs.shape
+    print costs[ind].shape
+    assert 0
     return costs[ind]
 
 def plot_gmm(train, test, gmm):
@@ -67,3 +77,24 @@ def plot_gmm(train, test, kde, nsamps, fs=5, filename='./plots/kde.png'):
             color='k', alpha=0.4)
     pl.plot(samps, scores, 'r', lw=2)
     fig.savefig(filename)
+
+def get_rand_features():
+    assert 0
+
+if __name__ == '__main__':
+
+    seed = 234
+    np.random.seed(seed)
+
+    df = pd.read_csv('./data/sparcs_cleaned.csv')
+    keys = list(df.columns.values)
+    
+    ind = np.random.randint(len(df.shape[0]))
+    features = {}
+    for k in keys:
+        features[k] = df[k][ind]
+    features.pop('Unnamed: 0')
+    features.pop('Total Charges')
+
+
+
